f0f91982d071bc0bf74c8f8e0c1e6216d35de826
migueXYLO <2221462@my.ipleiria.pt>
2024-03-01 12:38:57 +0000

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "Hello World!";
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
